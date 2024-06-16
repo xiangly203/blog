bd7881e2d58cb36d86607ce0a857badb2fe7c592
“xyi203” <xiangly203@outlook.com>
2024-06-16 23:29:32 +0800

    fix: update to next 15 rc

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -1,87 +1,81 @@
-"use client"
-import * as React from "react"
-import Link from "next/link"
+"use client";
+import * as React from "react";
+import Link from "next/link";
 
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
-import { useTheme } from "next-themes"
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { useTheme } from "next-themes";
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import {
-    NavigationMenu, NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    navigationMenuTriggerStyle,
-} from "@/components/ui/navigation-menu"
-import {MoonIcon, SunIcon} from "@radix-ui/react-icons";
-import { Separator } from "@/components/ui/separator"
-import Image from 'next/image'
-import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { Separator } from "@/components/ui/separator";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import XYi from "@/components/icons/personal";
+
 export function NavigationMenuDemo() {
-    const { setTheme } = useTheme()
-    return (
-        <div className="">
-            <NavigationMenu className="container w-1/2 ">
-                <NavigationMenuList className="grid grid-cols-12 gap-2">
-                    <NavigationMenuItem className="">
-
-                        <Link href="/">
-                           <XYi/>
-                        </Link>
-
-                    </NavigationMenuItem>
-                    <NavigationMenuItem className="">
-                        <Link href="/blog" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                博客
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem className="">
-                        <Link href="/about" legacyBehavior passHref>
-                            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
-                                关于我
-                            </NavigationMenuLink>
-                        </Link>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem className="col-start-9">
-                        <DropdownMenu>
-                            <DropdownMenuTrigger asChild>
-                                <Button variant="ghost" size="icon">
-                                    <SunIcon
-                                        className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"/>
-                                    <MoonIcon
-                                        className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"/>
-                                </Button>
-                            </DropdownMenuTrigger>
-                            <DropdownMenuContent align="end">
-                                <DropdownMenuItem onClick={() => setTheme("light")}>
-                                    Light
-                                </DropdownMenuItem>
-                                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                                    Dark
-                                </DropdownMenuItem>
-                                <DropdownMenuItem onClick={() => setTheme("system")}>
-                                    System
-                                </DropdownMenuItem>
-                            </DropdownMenuContent>
-                        </DropdownMenu>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem className="">
-                        <Avatar>
-                            {/*<AvatarImage src="https://github.com/shadcn.png" />*/}
-                            <AvatarFallback>XYi</AvatarFallback>
-                        </Avatar>
-                    </NavigationMenuItem>
-                </NavigationMenuList>
-            </NavigationMenu>
-            <Separator/>
-        </div>
-    );
+  const { setTheme } = useTheme();
+  return (
+    <div className="w-full">
+      <NavigationMenu className="container w-full px-4 sm:w-1/2 sm:px-0">
+        <NavigationMenuList className="flex flex-wrap gap-2">
+          <NavigationMenuItem className="flex-1 sm:flex-none sm:w-auto">
+            <Link href="/">
+              <XYi />
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="flex-1 sm:flex-none sm:w-auto">
+            <Link href="/blog" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                博客
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="flex-1 sm:flex-none sm:w-auto">
+            <Link href="/about" legacyBehavior passHref>
+              <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                关于我
+              </NavigationMenuLink>
+            </Link>
+          </NavigationMenuItem>
+          <NavigationMenuItem className="flex-1 sm:flex-none sm:w-auto">
+            {/* 调整排列布局 */}
+          </NavigationMenuItem>
+          <NavigationMenuItem className="ml-auto">
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                  <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuItem onClick={() => setTheme("light")}>
+                  Light
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("dark")}>
+                  Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setTheme("system")}>
+                  System
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+      <Separator />
+    </div>
+  );
 }
